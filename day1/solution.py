53d36e6f2df099f7a723bbe17c46026e50a68282
Olav Landsverk <olav.landsverk@gmail.com>
2025-12-01 13:50:12 +0100

    day 1

---
+++ day1/solution.py
@@ -0,0 +1,23 @@
+from typing import Iterator
+
+DIAL_SIZE = 100
+
+
+def parse_rotations_file(file_path: str) -> Iterator[int]:
+    # R48 -> 48, L31 -> -31
+    with open(file_path, "r") as file:
+        for line in file:
+            value = int(line[1:])
+            if line[0] == "L":
+                value = -value
+            yield value
+
+
+position = 50
+n_at_zero = 0
+for rotation in parse_rotations_file("input.txt"):
+    position = (position + rotation) % DIAL_SIZE
+    if position == 0:
+        n_at_zero += 1
+
+print(n_at_zero)
