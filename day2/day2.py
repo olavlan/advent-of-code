eb94dca679f352ebcbed614a0d279ac0e27b294c
Olav Landsverk <olav.landsverk@gmail.com>
2025-12-02 21:58:49 +0100

    Day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -53,3 +53,59 @@ def solve_part1():
 
 
 print(solve_part1())
+
+
+def get_divisors(n) -> Iterator[int]:
+    yield 1
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            yield i
+
+
+def generate_numbers_with_repetitions(
+    n_digits: int, start: str | None = None, stop: str | None = None
+) -> Iterator[int]:
+    if n_digits == 1:
+        return
+    if start is None:
+        start = "1" + "0" * (n_digits - 1)
+    if stop is None:
+        stop = "9" * n_digits
+    for repetition_length in get_divisors(n_digits):
+        n_repetitions = int(n_digits / repetition_length)
+        first_repeated_number = int(start[:repetition_length])
+        last_repeated_number = int(stop[:repetition_length])
+        first_number = int(str(first_repeated_number) * n_repetitions)
+        last_number = int(str(last_repeated_number) * n_repetitions)
+        if int(start) <= first_number <= int(stop):
+            yield int(first_number)
+        for number in range(first_repeated_number + 1, last_repeated_number):
+            yield int(str(number) * n_repetitions)
+        if int(start) <= last_number <= int(stop):
+            yield int(last_number)
+
+
+def generate_numbers_with_repetitions_in_range(start: str, stop: str) -> Iterator[int]:
+    n_digits_start = len(start)
+    n_digits_stop = len(stop)
+
+    if n_digits_start == n_digits_stop:
+        yield from generate_numbers_with_repetitions(
+            n_digits_start, start=start, stop=stop
+        )
+        return
+    yield from generate_numbers_with_repetitions(n_digits_start, start=start)
+    for n_digits in range(n_digits_start + 1, n_digits_stop):
+        yield from generate_numbers_with_repetitions(n_digits)
+    yield from generate_numbers_with_repetitions(n_digits_stop, stop=stop)
+
+
+def solve_part2():
+    invalid_ids: set[int] = set()
+    for start, stop in parse_id_ranges_file("input.txt"):
+        for invalid_id in generate_numbers_with_repetitions_in_range(start, stop):
+            invalid_ids.add(invalid_id)
+    return sum(invalid_ids)
+
+
+print(solve_part2())
